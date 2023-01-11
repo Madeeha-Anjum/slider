c43a900f2db76aef04ec41124501f6f4f58a7304
Madeeha <maadeeeehaa@gmail.com>
2023-01-11 10:56:29 -0700

    css modules :bulb:

--- src/components/SliderCssModules.jsx
+++ src/components/SliderCssModules.jsx
@@ -1,7 +1,40 @@
-import React from "react";
+import React, { useState } from "react";
+import styles from "./SliderCssModules.module.css";
+import Icons from "./icons/index";
+import { Link } from "react-router-dom";
 
 const SliderCssModules = () => {
-  return <div>SliderCssModules</div>;
+  const [isOpen, setIsOpen] = useState(false);
+  // conditionally render the slide class
+  const sliderClass = isOpen
+    ? `${styles.slider} ${styles.open}`
+    : `${styles.slider} ${styles.closed}`;
+
+  const toggleSlider = () => {
+    setIsOpen(!isOpen);
+  };
+  return (
+    <>
+      <div className={sliderClass}>
+        <div className={styles["menu-wrapper"]}>
+          <button
+            title="Home"
+            type="button"
+            onClick={() => {
+              toggleSlider();
+            }}
+            className={styles["menu-btn"]}
+          >
+            <Icons.Home style={{ width: "2em", height: "2em" }} />
+          </button>
+
+          <div className={styles.menu}>
+            <Link to={`/`}>Home</Link>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default SliderCssModules;
